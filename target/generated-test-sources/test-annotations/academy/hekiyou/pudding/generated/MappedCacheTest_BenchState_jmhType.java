b5378d223ed58cabf9acf190714f9fdf295087eb
tsunko <tsunko@protonmail.com>
2020-07-13 04:58:52 -0700

    Reapply .gitignore

--- target/generated-test-sources/test-annotations/academy/hekiyou/pudding/generated/MappedCacheTest_BenchState_jmhType.java
+++
@@ -1,4 +0,0 @@
-package academy.hekiyou.pudding.generated;
-public class MappedCacheTest_BenchState_jmhType extends MappedCacheTest_BenchState_jmhType_B3 {
-}
-
--- target/generated-test-sources/test-annotations/academy/hekiyou/pudding/generated/MappedCacheTest_jmhType.java
+++
@@ -1,4 +0,0 @@
-package academy.hekiyou.pudding.generated;
-public class MappedCacheTest_jmhType extends MappedCacheTest_jmhType_B3 {
-}
-
